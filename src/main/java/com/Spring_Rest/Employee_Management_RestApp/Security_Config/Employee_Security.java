8923cfd2d5d8506f8f7143824b1001d87a5019ed
adarshnidhankar <adarshnid123@gmail.com>
2023-08-15 11:04:10 +0530

    second commit

---
+++ src/main/java/com/Spring_Rest/Employee_Management_RestApp/Security_Config/Employee_Security.java
@@ -0,0 +1,10 @@
+package com.Spring_Rest.Employee_Management_RestApp.Security_Config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Employee_Security {
+    public void user(){
+
+    }
+}
