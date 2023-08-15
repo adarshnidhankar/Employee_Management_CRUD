306ca980f2c02252033b99eb7a977c6734fe7c0c
adarshnidhankar <adarshnid123@gmail.com>
2023-08-15 11:08:23 +0530

    second commit

--- src/main/java/com/Spring_Rest/Employee_Management_RestApp/Security_Config/Employee_Security.java
+++ src/main/java/com/Spring_Rest/Employee_Management_RestApp/Security_Config/Employee_Security.java
@@ -1,10 +1,26 @@
 package com.Spring_Rest.Employee_Management_RestApp.Security_Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class Employee_Security {
-    public void user(){
+    @Bean
+    public UserDetailsService user(){
+        UserDetails userDetails = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("123")).roles("user").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
